d8ff9d17f56bdd0988ee48ed43beaef82fc4a023
avadh <avdhasakariya@gmail.com>
2023-06-22 09:48:19 +0530

    hello

---
+++ New folder/for.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,sp=25;
+    {
+        for(i=1;i<=sp;i++)
+        cout<<" ";
+        for(j=1;j<=1;j++){
+            cout<<"#&";
+        }
+    }
+}
