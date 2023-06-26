feab260ecdd37dce2ca05f36859d3bd084229e70
avadh <avdhasakariya@gmail.com>
2023-06-26 09:23:30 +0530

    av

---
+++ New folder/a5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Test{
+    private:
+    int a;
+    public:
+    void setter(){
+        cout<<"Enter a:";
+        cin>>a;
+    }
+    void getter(){
+        cout<<"value of a:"<<a<<endl;
+    }
+};
+int main(){
+  Test z;
+  z.setter();
+  z.getter();
+
+}
